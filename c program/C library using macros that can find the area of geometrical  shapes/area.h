5c17653158b2175abafd9c776eb37784b40c6286
Lisban Gonsalves <63594467+lisbangonsalves@users.noreply.github.com>
2023-04-24 15:38:34 +0530

    final

--- c program/C library using macros that can find the area of geometrical  shapes/area.h
+++
@@ -1,6 +0,0 @@
-#define PI 3.14159265359
-
-#define AREA_RECTANGLE(length, width) ((length)*(width))
-#define AREA_SQUARE(side) ((side)*(side))
-#define AREA_CIRCLE(radius) (PI*(radius)*(radius))
-#define AREA_TRIANGLE(base, height) ((base)*(height)/2)
